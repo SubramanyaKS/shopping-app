d5d815dbc3b03d1a5ffabef34beadfa0f0dee83c
Subramanya K S <subramanyaks2205@gmail.com>
2023-10-22 11:07:39 +0530

    updated 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const ErrorPage = () => {
+  return (
+    <div className="not-found">
+      <h2 className="display-1">404</h2>
+      <h5>Oops!!! Page Not Found</h5>
+      <Link to="/">Back to Home Page</Link>
+    </div>
+  )
+}
+
+export default ErrorPage

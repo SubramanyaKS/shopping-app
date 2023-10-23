971efe6c9c91c4e0b5146fd5d348eacc5877b34f
Subramanya K S <subramanyaks2205@gmail.com>
2023-10-23 19:28:59 +0530

    Added No Cart

---
+++ src/components/NoCart.jsx
@@ -0,0 +1,20 @@
+import { Cart4 } from 'react-bootstrap-icons'
+import { useNavigate } from "react-router-dom";
+
+const NoCart = () => {
+    const navigate = useNavigate();
+  return (
+    <div className='no-cart'>
+        <Cart4  style={{
+          width: '50%', 
+          height: '100px',
+          margin:'1%',
+        }}/>
+        <h5>There is no items in your cart.</h5>
+        <h6>Please add the products which you like.</h6>
+        <button className="p-2 m-2 bg-primary text-light border-0 rounded-pill " onClick={()=>navigate('/product')}>Add the Product</button>
+    </div>
+  )
+}
+
+export default NoCart

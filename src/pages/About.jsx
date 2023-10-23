95312396ba0444fae4f413eac2c4a313f2d7f87f
Subramanya K S <subramanyaks2205@gmail.com>
2023-10-23 19:28:44 +0530

    added about

---
+++ src/pages/About.jsx
@@ -0,0 +1,33 @@
+
+const About = () => {
+  return (
+    <div>
+      <h2 className="text-center p-2">About Us</h2>
+      <p className="p-3">
+        Welcome to NamKart, your one-stop shop for all your shopping needs. We
+        are dedicated to providing the best shopping experience with a wide
+        range of high-quality products and exceptional customer service.
+      </p>
+      <h4 className="text-center p-2">Our Mission</h4>
+      <p className="p-3">
+        our mission is to make your online shopping experience as convenient and
+        enjoyable as possible. We aim to connect you with the finest products,
+        from fashion to electronics, and deliver them
+      </p>
+      <h4 className="text-center p-2">Get in Touch</h4>
+      <p className="p-3 pb-0">
+        We love to hear from our customers. If you have any questions,
+        suggestions, or feedback, please don &apos;t hesitate to contact us. You can
+        reach us via email at support@namkart.com or follow us on social media
+        for updates and promotions.
+      </p>
+      <p className="p-3 pb-0 pt-0">
+        Thank you for choosing us . We look forward to serving you on your
+        shopping journey
+      </p>
+      <p className="p-3 pt-0">Happy shopping!</p>
+    </div>
+  );
+};
+
+export default About;

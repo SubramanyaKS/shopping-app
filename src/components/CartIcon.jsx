04b0bccf3dbfb2d63ad8ca34c7d5967c92d0758d
Subramanya K S <subramanyaks2205@gmail.com>
2023-10-26 18:48:55 +0530

    added carticon

---
+++ src/components/CartIcon.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+
+import { CartFill } from 'react-bootstrap-icons';
+import { Badge } from 'react-bootstrap';
+
+const CartIcon = ({ itemCount }) => {
+  return (
+    <div className="cart-icon">
+      <CartFill className='mt-2'/>
+      <Badge pill variant="danger" className="cart-badge">
+        {itemCount}
+      </Badge>
+    </div>
+  );
+};
+
+export default CartIcon;

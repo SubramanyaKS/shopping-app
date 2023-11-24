252075f899fc69cfed03762b8a471c2461a250a0
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-25 00:56:19 +0530

    updated

---
+++ src/components/TopRated.jsx
@@ -0,0 +1,25 @@
+import { useProductContext } from "../contexts/ProductContext";
+import ProductCard from "./ProductCard"
+
+const TopRated = () => {
+    const { products } = useProductContext();
+    // console.log(products);
+    const selectTopRatedProducts = (products,minRating) => {
+        return products.filter(product => product.rating.rate >= minRating).slice(0, 4);
+    };    
+      
+    const featuredProducts = selectTopRatedProducts(products,4.5);
+
+  return (
+    <div>
+        <h3 className="text-decoration-underline text-center">Top Rated Products</h3>
+        <div className="product-list mt-3">
+        {featuredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default TopRated

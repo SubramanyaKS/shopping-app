7e5eee8bbb4bfd4e565946d66eb77c4dc4d26cf1
Subramanya K S <subramanyaks2205@gmail.com>
2023-12-04 06:29:59 +0530

    updated searchProducts

---
+++ src/components/SearchProducts.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import { useProductSearch } from '../contexts/ProductSearchContext'
+import ProductCard from './ProductCard';
+
+const SearchProducts = () => {
+  const {searchState} = useProductSearch();
+  return (
+    <div>
+    <h3  className="text-decoration-underline text-center">Search</h3>
+    <div className="product-list mt-3">
+    {searchState.products.map((product) => (
+      <ProductCard key={product.id} product={product}/>
+    ))}
+    </div>
+  </div>
+  )
+}
+
+export default SearchProducts

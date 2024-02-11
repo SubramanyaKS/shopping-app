d4adaeb6f042a1984feb2a5d77f8131a9b5f51c4
Subramanya K S <subramanyaks2205@gmail.com>
2024-02-11 07:05:23 +0530

    updated main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,8 @@
 import { Col, Container, Image, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import img from '../assets/men.png';
-
+import { ChevronRight } from "react-bootstrap-icons";
+import { mainpagedata } from "../utils/data";
 const Main = () => {
   const navigate = useNavigate();
   return (
@@ -9,9 +10,9 @@ const Main = () => {
         <Container>
           <Row>
             <Col>
-            <h1 className="fs-1 text-primary text-justify p-3">Discover, Shop, and Experience a World of Products at Your Fingertips </h1>
-            <h6 className="lh-lg p-3 text-justify">Welcome to our e-commerce paradise, where you &apos; ll find a curated selection of the latest trends in fashion, electronics, home decor, and more. Our mission is to make online shopping a delightful experience, offering high-quality products, seamless transactions, and exceptional customer service. Join us on this shopping journey and elevate your lifestyle with every click</h6>
-            <button onClick={()=>navigate("/product")} className="product-btn">Shop Now -&gt;</button>
+            <h1 className="fs-1 text-primary text-justify p-3">{mainpagedata.heading}</h1>
+            <h6 className="lh-lg p-3 text-justify">{mainpagedata.details}</h6>
+            <button onClick={()=>navigate("/product")} className="product-btn">Shop Now <ChevronRight /></button>
             </Col>
             <Col>
               <Image src ={img} alt="model" style={{ maxWidth: '100%', height: 'auto' }}/>

47e558344e4631edd63e560dec4b6fbcdd4833b8
Subramanya K S <subramanyaks2205@gmail.com>
2024-02-19 07:19:09 +0530

    modified style part

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 
-const TextBox = ({ placeholder, value, onChange,type,pattern="" }) => {
+const TextBox = ({ placeholder, value, onChange,type,icon}) => {
 
   return (
     <div className="text-box">
-      <input type={type} className="custom-input" onChange={onChange} value={value} placeholder={placeholder} pattern={pattern} />
+      <input type={type} className="custom-input" onChange={onChange} value={value} placeholder={placeholder}  />
+      {icon && <span className="icon">{icon}</span>}
     </div>
   )
 }

27d51d5f704c410a4ea6829e9701e6aae287ae9b
Subramanya K S <subramanyaks2205@gmail.com>
2024-02-20 21:48:24 +0530

    added data

---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+export const mainpagedata={
+    heading:"Discover, Shop, and Experience a World of Products at Your Fingertips.",
+    details:"Welcome to our e-commerce paradise, where you' ll find a curated selection of the latest trends in fashion, electronics, home decor, and more. Our mission is to make online shopping a delightful experience, offering high-quality products, seamless transactions, and exceptional customer service. Join us on this shopping journey and elevate your lifestyle with every click."
+}
+
+export const aboutdata ={
+    title:"About NammKart",
+    details:"Welcome to NammKart, your one-stop shop for all your shopping needs. We are dedicated to providing the best shopping experience with a wide range of high-quality products and exceptional customer service.",
+    getintouch:" We love to hear from our customers. If you have any questions, suggestions, or feedback, please don't hesitate to contact us. You can reach us via email at support@namkart.com or follow us on social media for updates and promotions.",
+    mission:"our mission is to make your online shopping experience as convenient and enjoyable as possible. We aim to connect you with the finest products, from fashion to electronics, and deliver them",
+    thanks:"Thank you for choosing us . We look forward to serving you on your shopping journey"
+}

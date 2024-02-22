e3a3a211dd920badc0d526899c865c5f0e73d08e
Subramanya K S <subramanyaks2205@gmail.com>
2024-02-22 06:55:29 +0530

    updated error page with styles

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const ErrorPage = () => {
   return (
     <div className="not-found">
-      <h2 className="display-1">404</h2>
+      <h2 className="error-code">404</h2>
       <h5>Oops!!! Page Not Found</h5>
       <Link to="/">Back to Home Page</Link>
     </div>

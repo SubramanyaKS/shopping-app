8fbfd88b54cf5e8de2087f7588e74ca59622adf1
Subramanya K S <subramanyaks2205@gmail.com>
2024-03-20 06:42:15 +0530

    modified toast

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,14 +1,31 @@
 import { toast } from "react-toastify";
 
 export const showCustomToast = (message,type) => {
-    toast(message, {
-      position: 'top-right',
-      autoClose: 2000, // Auto close after 2 seconds
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: false,
-      draggable: true,
-      progress: undefined,
-      className: type, // Add a custom class for styling
-    });
+    if(type=="success-toast"){
+      toast.success(message, {
+        position: 'top-right',
+        autoClose: 2000, // Auto close after 2 seconds
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        theme:"colored",
+        progress: undefined,
+        // className: type, // Add a custom class for styling
+      });
+    }
+    else{
+      toast.error(message, {
+        position: 'top-right',
+        theme:"colored",
+        hideProgressBar:false,
+        autoClose: 2000, // Auto close after 2 seconds
+        // hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        // className: type, // Add a custom class for styling
+      });
+    }
   };

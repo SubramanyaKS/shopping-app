a291993f3545ef8333947c96cb84b701b7199b5e
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-02 07:11:12 +0530

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,22 @@
+import { aboutdata } from "../utils/data";
 
 const About = () => {
   return (
     <div>
-      <h2 className="text-center p-2">About Us</h2>
+      <h2 className="text-center text-cursive text-bluepurple p-2">{aboutdata.title}</h2>
       <p className="p-3">
-        Welcome to NamKart, your one-stop shop for all your shopping needs. We
-        are dedicated to providing the best shopping experience with a wide
-        range of high-quality products and exceptional customer service.
+        {aboutdata.details}
       </p>
-      <h4 className="text-center p-2">Our Mission</h4>
+      <h4 className="text-center text-cursive p-2">Our Mission</h4>
       <p className="p-3">
-        our mission is to make your online shopping experience as convenient and
-        enjoyable as possible. We aim to connect you with the finest products,
-        from fashion to electronics, and deliver them
+        {aboutdata.mission}
       </p>
-      <h4 className="text-center p-2">Get in Touch</h4>
+      <h4 className="text-center text-cursive p-2">Get in Touch</h4>
       <p className="p-3 pb-0">
-        We love to hear from our customers. If you have any questions,
-        suggestions, or feedback, please don &apos;t hesitate to contact us. You can
-        reach us via email at support@namkart.com or follow us on social media
-        for updates and promotions.
+        {aboutdata.getintouch}
       </p>
       <p className="p-3 pb-0 pt-0">
-        Thank you for choosing us . We look forward to serving you on your
-        shopping journey
+       {aboutdata.thanks}
       </p>
       <p className="p-3 pt-0">Happy shopping!</p>
     </div>

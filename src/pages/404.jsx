f57249c1b585e7a6cf6882db5f9702bad96cb1c0
Subramanya K S <subramanyaks2205@gmail.com>
2024-07-02 22:48:41 +0530

    updated 404 pages

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,10 +4,13 @@ const ErrorPage = () => {
   return (
     <div className="not-found">
       <h2 className="error-code">404</h2>
-      <h5>Oops!!! Page Not Found</h5>
+      <h5>Oops!!! Page not found</h5>
+      <p>
+        The page you are looking for doesnot exist or has been moved.Please click
       <Link to="/">Back to Home Page</Link>
+      </p>
     </div>
-  )
+  );
 }
 
 export default ErrorPage
